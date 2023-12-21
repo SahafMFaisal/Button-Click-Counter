032c88cf4aeb0fd5aa26b0f7318c5adb51dc3b2c
SAHAF M FAISAL <133376398+SahafMFaisal@users.noreply.github.com>
2023-12-21 19:05:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,11 @@
     const button = document.getElementById("clickButton");
     const clickCountDisplay = document.getElementById("clickCount");
 
-    // Array of colors to cycle through
     const colors = ["yellow", "violet", "pink"];
 
-    // Function to handle button click
     function buttonClick() {
-        // Increment click count
         clickCount++;
-        // Update click count display
         clickCountDisplay.textContent = clickCount;
-
-        // Change button color
         button.style.backgroundColor = colors[colorIndex];
-
-        // Cycle through colors
-        colorIndex = (colorIndex + 1) % colors.length;
+        colorIndex = (colorIndex + 0) % colors.length;
     }
