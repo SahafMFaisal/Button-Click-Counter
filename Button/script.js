198a9ea853d5a30a73e93f5767f925d5f880efcd
SAHAF M FAISAL <133376398+SahafMFaisal@users.noreply.github.com>
2023-12-21 18:31:55 +0530

    Add files via upload

---
+++ Button/script.js
@@ -0,0 +1,20 @@
+
+
+let clickCount = 0;
+let colorIndex = 0;
+const button = document.getElementById("clickButton");
+const clickCountDisplay = document.getElementById("clickCount");
+
+const colors = ["yellow", "violet", "pink"];
+
+function buttonClick() {
+
+    clickCount++;
+    clickCountDisplay.textContent = clickCount;
+
+    button.style.backgroundColor = colors[colorIndex];
+
+    colorIndex = (colorIndex + 1) % colors.length;
+}
+
+button.addEventListener("click", buttonClick);
